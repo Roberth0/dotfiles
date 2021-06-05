7dad1da9a07efd85ed5f562e24262dec4a5f3508
Roberth0 <56902965+Roberth0@users.noreply.github.com>
2021-06-04 21:45:52 -0500

    Update postBaseInstall.sh

--- postBaseInstall.sh
+++ postBaseInstall.sh
@@ -36,7 +36,7 @@ systemctl enable NetworkManager
 echo "network configured"
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-grub-mkconfig -o /bott/grub/grub.cfg
+grub-mkconfig -o /boot/grub/grub.cfg
 echo "bootloader configured"
 
 useradd -mG wheel itami
